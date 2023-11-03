3f3070824393eb9e83ba606d5430ba54ffbf8f39
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-11-03 17:19:53 +0100

    mail rcp project

---
+++ emf.parsley.demo.mail.rcp/src/org/eclipse/emf/parsley/examples/mail/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2013 RCP Vision (http://www.rcp-vision.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Lorenzo Bettini - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.parsley.examples.mail.rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * This workbench advisor creates the window advisor, and specifies
+ * the perspective id for the initial window.
+ */
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+	
+    @Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return Perspective.ID;
+	} 
+	
+}

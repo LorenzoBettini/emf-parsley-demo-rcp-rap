c2d189dd98564588cb293175e9cbf8a317e971ce
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-11-04 16:45:09 +0100

    adapted RAP project

--- emf.parsley.demo.mail.rap/src/org/eclipse/emf/parsley/examples/mail/rcp/MailApplication.java
+++ emf.parsley.demo.mail.rap/src/org/eclipse/emf/parsley/examples/mail/rcp/MailApplication.java
@@ -13,46 +13,23 @@ package org.eclipse.emf.parsley.examples.mail.rcp;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.application.WorkbenchAdvisor;
 
 /**
  * This class controls all aspects of the application's execution
  */
 public class MailApplication implements IApplication {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
-	 */
 	@Override
-	public Object start(IApplicationContext context) {
+	public Object start(IApplicationContext context) throws Exception {
 		Display display = PlatformUI.createDisplay();
-		try {
-			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-			if (returnCode == PlatformUI.RETURN_RESTART) {
-				return IApplication.EXIT_RESTART;
-			}
-			return IApplication.EXIT_OK;
-		} finally {
-			display.dispose();
-		}
+		WorkbenchAdvisor advisor = new ApplicationWorkbenchAdvisor();
+		return PlatformUI.createAndRunWorkbench(display, advisor);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
 	@Override
 	public void stop() {
-		if (!PlatformUI.isWorkbenchRunning())
-			return;
-		final IWorkbench workbench = PlatformUI.getWorkbench();
-		final Display display = workbench.getDisplay();
-		display.syncExec(new Runnable() {
-			@Override
-			public void run() {
-				if (!display.isDisposed())
-					workbench.close();
-			}
-		});
+		// Do nothing
 	}
 }

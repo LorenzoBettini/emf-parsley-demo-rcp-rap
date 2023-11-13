23b7c7c3ac539270dedf45db08435ea7849c7592
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-11-13 13:51:34 +0100

    removed old comments from MailApplication RAP

--- rap/emf.parsley.demo.mail.rap/src/emf/parsley/demo/mail/app/rap/MailApplication.java
+++ rap/emf.parsley.demo.mail.rap/src/emf/parsley/demo/mail/app/rap/MailApplication.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2013 RCP Vision (http://www.rcp-vision.com) and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- * Lorenzo Bettini - initial API and implementation
- *******************************************************************************/
 package emf.parsley.demo.mail.app.rap;
 
 import org.eclipse.equinox.app.IApplication;
